6fb17ee84b3fbc78099fe2ad6e21392a1ce53d32
Tommy MArtin <tomjones02@gmail.com>
2022-01-24 13:52:00 -0500

    Revert "Added Darker theme"
    This reverts commit 3505549d20d041fee77931683da9f8794b14be38.

--- Ayu-TrueMirage/Properties/AssemblyInfo.cs
+++ Ayu-TrueMirage/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ayu_TrueMirage")]
+[assembly: AssemblyTitle("Ayu TrueMirage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ayu_TrueMirage")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("Ayu_TrueMirage.Properties")]
+[assembly: AssemblyCopyright("TomGUN")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,6 +19,9 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9cb79d32-d4aa-4fd2-9dc5-a3175cad8f2a")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -26,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
